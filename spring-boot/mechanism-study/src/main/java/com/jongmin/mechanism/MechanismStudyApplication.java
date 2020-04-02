317f231019b7280c3aed8f4c0b4032988803baf8
jm-kim <jm-kim@linecorp.com>
2020-04-03 00:07:44 +0900

    spring-boot mechanism-study init

---
+++ spring-boot/mechanism-study/src/main/java/com/jongmin/mechanism/MechanismStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jongmin.mechanism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanismStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MechanismStudyApplication.class, args);
+    }
+
+}
---
+++ spring-boot/mechanism-study/src/test/java/com/jongmin/mechanism/MechanismStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongmin.mechanism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechanismStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
