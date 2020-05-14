5d0d7dda2a48877b515c3050479553e5c15ab069
jongmin92 <kdhx92@gmail.com>
2020-05-14 22:53:51 +0900

    Add spring/spring-core - 1.8.1. Customizing Beans by Using a BeanPostProcessor

---
+++ spring/spring-core/src/main/java/com/jongmin/springcore/InstantiationTracingBeanPostProcessor.java
@@ -0,0 +1,18 @@
+package com.jongmin.springcore;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class InstantiationTracingBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(beanName + " is created");
+        return bean;
+    }
+}
