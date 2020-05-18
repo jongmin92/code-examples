4c50a8434c3f28390de1641e10f71c43c16d402b
jongmin92 <kdhx92@gmail.com>
2020-05-19 01:44:35 +0900

    Add spring/spring-core - 1.12.1. Basic Concepts: @Bean and @Configuration

---
+++ spring/spring-core/src/main/java/com/jongmin/springcore/AppConfig.java
@@ -0,0 +1,13 @@
+package com.jongmin.springcore;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MyService myService() {
+        return new MyServiceImpl();
+    }
+}
--- spring/spring-core/src/test/java/com/jongmin/springcore/ApplicationTest.java
+++ spring/spring-core/src/test/java/com/jongmin/springcore/ApplicationTest.java
@@ -1,13 +1,28 @@
 package com.jongmin.springcore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class ApplicationTest {
+
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void basicConceptsTest() {
+        final AppConfig appConfig = context.getBean(AppConfig.class);
+        final MyService service = context.getBean(MyService.class);
+
+        assertThat(appConfig).isNotNull();
+        assertThat(service).isNotNull();
+    }
 }
