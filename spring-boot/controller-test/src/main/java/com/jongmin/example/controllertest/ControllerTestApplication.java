bebbbbc53044e229d5bbcd02e36e57f95280df7e
jongmin92 <kdhx92@gmail.com>
2020-05-25 21:30:41 +0900

    Init spring-boot/controller-test

---
+++ spring-boot/controller-test/src/main/java/com/jongmin/example/controllertest/ControllerTestApplication.java
@@ -0,0 +1,13 @@
+package com.jongmin.example.controllertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerTestApplication.class, args);
+    }
+
+}
