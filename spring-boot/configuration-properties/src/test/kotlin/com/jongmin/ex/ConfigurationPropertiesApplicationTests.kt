34280a4c5865baf675d06ee0b9bcb9b369fd3557
jongmin92 <kdhx92@gmail.com>
2020-06-23 23:24:04 +0900

    Init spring-boot/configuration-properties

---
+++ spring-boot/configuration-properties/src/test/kotlin/com/jongmin/ex/ConfigurationPropertiesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jongmin.ex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationPropertiesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
